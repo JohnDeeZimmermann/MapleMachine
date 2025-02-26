b59d89c0481ee26de6e704248aa2b39769332692
JohnDeeZimmermann <johndee.zimmermann@gmail.com>
2025-02-26 21:09:46 +0100

    Refine error message format in AssemblyError exception

--- src/main/java/de/johndee/maple/exceptions/AssemblyError.java
+++ src/main/java/de/johndee/maple/exceptions/AssemblyError.java
@@ -3,7 +3,7 @@ package de.johndee.maple.exceptions;
 public class AssemblyError extends RuntimeException {
 
     public AssemblyError(int lineNumber, String line, String error) {
-        super("An error occurred at line " + lineNumber + ": '" + line + "' - " + error);
+        super("An error occurred at line " + lineNumber + ": '" + line + "'; ERROR: " + error);
     }
 
 }
